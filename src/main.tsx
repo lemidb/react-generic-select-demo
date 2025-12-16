e0c68b6d0f7c677327bd272797b980d94cf52169
Lemi Melkamu <lemim@ethiopianairlines.com>
2025-12-16 14:59:54 +0300

    Fix: remove unused sidebar provider defined in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
-import { SidebarProvider } from "./components/ui/sidebar.tsx";
 
 const queryClient = new QueryClient();
 
@@ -12,9 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        {/* <SidebarProvider> */}
           <App />
-        {/* </SidebarProvider> */}
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
